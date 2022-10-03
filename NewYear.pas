d7bdbaafddfd46cfba71c2d1f0326538740fe7d4
artermcherneczkij04@yandex.ru <user@mil8a-102a-01.mmcs.sfedu.ru>
2022-10-03 16:51:38 +0300

    NewYear

---
+++ NewYear.pas
@@ -0,0 +1,28 @@
+﻿program task3;
+begin
+  var (n,m):=ReadInteger2('введите месяц и число');
+  var (k,f):=ReadInteger2('введите месяц и число');
+  if n>k then
+    begin
+      println('к новому году ближе месяц:',n,'и','день',m);
+      exit;
+    end
+  else
+    begin
+      println('к новому году ближе месяц:',k,'и','день',f);
+      exit;
+    end;
+  if n=k then
+  begin
+    if m>f then
+      begin
+        println('к новому году ближе месяц:',n,'и','день',m);
+        exit;
+      end
+    else
+      begin
+        println('к новому году ближе месяц:',k,'и','день',f);
+        exit;
+      end;
+  end;
+end.
