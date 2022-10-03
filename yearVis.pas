ef2a5eb0175f3348823dc64cd1f9671a89af22a2
artermcherneczkij04@yandex.ru <user@mil8a-102a-01.mmcs.sfedu.ru>
2022-10-03 16:40:39 +0300

    yaerVis

---
+++ yearVis.pas
@@ -0,0 +1,8 @@
+﻿program task1;
+begin
+  var n:=ReadInteger('введите год');
+  if (n mod 4 =0) or (n mod 100>0) and (n mod 400 =0) then
+    println('високосный')
+  else
+    println('невисокосный');
+end.
