0128f2695c57641c4b6cb6cdf78ce3231e4c867c
artermcherneczkij04@yandex.ru <user@mil8a-102a-01.mmcs.sfedu.ru>
2022-10-03 16:58:17 +0300

    days

---
+++ days.pas
@@ -0,0 +1,9 @@
+﻿program task5;
+begin
+  var n:=Readinteger('введите год');
+  Assert(n>0,'ошибка');
+  if (n mod 4=0) or (n mod 100>0) and (n mod 400=0) then
+    println(366)
+  else
+    println(365);
+end.
